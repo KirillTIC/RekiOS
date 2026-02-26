5bb8341f16208eafa8f2789ba51d277788d6493b
KirillTIC <123kirillpuz123@gmail.com>
2026-02-26 16:49:55 +0700

    Keyboard and timer

--- src/drivers/keyboard.rs
+++ src/drivers/keyboard.rs
@@ -25,7 +25,7 @@ pub extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: Interrupt
         if let Some(key) = keyboard.process_keyevent(key_event) {
             match key {
                 DecodedKey::Unicode(character) => print!("{}", character),
-                DecodedKey::RawKey(key) => print!("{:?}", key),
+                DecodedKey::RawKey(key) => {}
             }
         }
     }

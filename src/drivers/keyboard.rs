b1e090d00e3e716aad3653e533c123dc80eccdf4
KirillTIC <123kirillpuz123@gmail.com>
2026-02-26 16:44:07 +0700

    Keyboard

--- src/drivers/keyboard.rs
+++ src/drivers/keyboard.rs
@@ -3,9 +3,32 @@ use crate::print;
 use x86_64::structures::idt::InterruptStackFrame;
 
 pub extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    let scancode: u8 = unsafe { x86_64::instructions::port::Port::new(0x60).read() };
-    //print!("{:#x} ", scancode);
-    print!("k");
+    use lazy_static::lazy_static;
+    use pc_keyboard::{DecodedKey, HandleControl, Keyboard, ScancodeSet1, layouts};
+    use spin::Mutex;
+    use x86_64::instructions::port::Port;
+
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> =
+            Mutex::new(Keyboard::new(
+                ScancodeSet1::new(),
+                layouts::Us104Key,
+                HandleControl::Ignore
+            ));
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
+        }
+    }
 
     unsafe {
         pic::PICS

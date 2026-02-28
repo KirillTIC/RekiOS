cab1244e02256d5e40a85d838e296d28fd199fed
KirillTIC <123kirillpuz123@gmail.com>
2026-02-28 16:49:30 +0700

    Input

--- src/drivers/timer.rs
+++ src/drivers/timer.rs
@@ -1,8 +1,6 @@
-use crate::print;
 use x86_64::structures::idt::InterruptStackFrame;
 
 pub extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    print!(".");
     unsafe {
         crate::arch::pic::PICS
             .lock()

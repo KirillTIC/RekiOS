c121653550ffb0838436d23c039507d3a9b92627
KirillTIC <123kirillpuz123@gmail.com>
2026-03-11 18:57:30 +0700

    Userspace full work

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate alloc;
 use bootloader_api::config::Mapping;
 use bootloader_api::{BootInfo, BootloaderConfig, entry_point};
 use reki_os::shell::interpreter;
-use reki_os::{clear, hlt_loop, println};
+use reki_os::{hlt_loop, println};
 
 pub static BOOTLOADER_CONFIG: BootloaderConfig = {
     let mut config = BootloaderConfig::new_default();
@@ -18,7 +18,6 @@ entry_point!(kernel_main, config = &BOOTLOADER_CONFIG);
 fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
     reki_os::init(boot_info);
 
-    clear!();
     println!(
         "Reki OS | Copyright (c) 2026 czeplenok -- MIT License\n{}",
         interpreter::fetch()
